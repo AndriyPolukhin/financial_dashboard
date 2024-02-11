b2552cb38dee7238f85a19bb0cb2aa14b288b831
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-11 17:14:17 +0200

    Updated Routes to render scenes for dashboard and predictions pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { useMemo } from 'react'
 import { themeSettings } from './theme'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '@/scenes/navbar'
+import Dashboard from './scenes/dashboard'
+import Predictions from './scenes/predictions'
 
 const App = () => {
 	const theme = useMemo(() => createTheme(themeSettings), [])
@@ -16,11 +18,8 @@ const App = () => {
 						<Box width='100%' height='100%' padding='1rem 2rem 4rem 2rem'>
 							<Navbar />
 							<Routes>
-								<Route path='/' element={<div>dashboard page</div>} />
-								<Route
-									path='/predictions'
-									element={<div>predictions page</div>}
-								/>
+								<Route path='/' element={<Dashboard />} />
+								<Route path='/predictions' element={<Predictions />} />
 							</Routes>
 						</Box>
 					</ThemeProvider>
